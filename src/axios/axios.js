6917c69972b623dc3327bdd4710b97a38a546a2c
Abhishek Patel <abhishekpatel946@gmail.com>
2020-11-16 20:59:05 +0530

    commit for push code on github

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   // the API (cloud function) URL
   baseURL: "http://localhost:5001/unofficial-5cad3/us-central1/api",
+
+  // TODO: upgrade the plan with blaze then 'deploy functions' && 'npm run build' && 'firebase deploy'.
 });
 
 export default instance;
